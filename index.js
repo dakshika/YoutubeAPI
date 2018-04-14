f724d189d8046ace29d749b4e26c083b4ef239b6
reger-men <adil-lashab@hotmail.com>
2018-04-14 22:55:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -210,6 +210,8 @@ app.get('/search/:query', function(req, res) {
 });
 
 //////////////////////////////////////////////////////////////////////////
+var serveIndex = require('serve-index');
+
 // Start the application!
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
